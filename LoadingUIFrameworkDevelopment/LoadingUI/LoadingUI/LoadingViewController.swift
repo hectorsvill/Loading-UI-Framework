4d53361819ca2a166db704e90f1a45ece3979c7f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-26 13:13:12 -0700

    view controller

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Hector Steven on 6/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+open class LoadingViewController: UIViewController {
+
+	override open func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+
+}
